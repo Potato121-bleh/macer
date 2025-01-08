930b9298f4520dc8e5b1f947f8dafe70ef6fe6a6
kimly07 <kimlyra55@gmail.com>
2025-01-08 09:08:49 +0700

    mer background

--- keyboardApp/urls.py
+++ keyboardApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns=[
-    path('home', views.home_page)
+    path('home', views.home_page),
+    path('login', views.login_page),
+    path('signup', views.signup_page)
 ]
